3a0cb1b2a6ee591f70ab9004a8afd35604466895
David Mzareulyan <david@hiero.ru>
2014-05-25 10:20:01 +0400

    Cross platform guessWidth()

--- usage.go
+++ usage.go
@@ -5,23 +5,8 @@ import (
 	"fmt"
 	"go/doc"
 	"io"
-	"os"
 	"strings"
-	"syscall"
-	"unsafe"
 )
-
-func guessWidth(w io.Writer) int {
-	if t, ok := w.(*os.File); ok {
-		fd := t.Fd()
-		var dimensions [4]uint16
-
-		if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(fd), uintptr(syscall.TIOCGWINSZ), uintptr(unsafe.Pointer(&dimensions)), 0, 0, 0); err == 0 {
-			return int(dimensions[1])
-		}
-	}
-	return 80
-}
 
 func (c *Application) Usage(w io.Writer) {
 	c.writeHelp(guessWidth(w), w)
