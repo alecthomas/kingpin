d865d8a060dd64756af02444b6685dc5231df5ae
Alec Thomas <alec@swapoff.org>
2014-08-27 10:42:26 +1000

    Actually hide hidden flags.

--- usage.go
+++ usage.go
@@ -109,7 +109,9 @@ func (f *flagGroup) writeHelp(width int, w io.Writer) {
 
 	rows := [][2]string{}
 	for _, flag := range f.flagOrder {
-		rows = append(rows, [2]string{formatFlag(flag), flag.help})
+		if !flag.hidden {
+			rows = append(rows, [2]string{formatFlag(flag), flag.help})
+		}
 	}
 	formatTwoColumns(w, 2, 2, width, rows)
 }
