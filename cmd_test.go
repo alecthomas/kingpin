de11bc42328f5bf434e3af92c025156175935227
Pablo Lalloni <plalloni@gmail.com>
2014-11-14 20:23:49 -0300

    Add failing test

--- cmd_test.go
+++ cmd_test.go
@@ -37,3 +37,19 @@ func TestNestedCommandsWithArgs(t *testing.T) {
 	assert.Equal(t, "c", *a)
 	assert.Equal(t, "d", *b)
 }
+
+func TestNestedCommandsWithFlags(t *testing.T) {
+	app := New("app", "")
+	cmd := app.Command("a", "").Command("b", "")
+	a := cmd.Flag("aaa", "").Short('a').String()
+	b := cmd.Flag("bbb", "").Short('b').String()
+	err := app.init()
+	assert.NoError(t, err)
+	context := Tokenize([]string{"a", "b", "--aaa", "x", "-b", "x"})
+	selected, err := app.parse(context)
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(context.Tokens))
+	assert.Equal(t, "a b", selected)
+	assert.Equal(t, "x", *a)
+	assert.Equal(t, "x", *b)
+}
